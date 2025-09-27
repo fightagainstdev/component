8dec3fd3d88383f554b30d33fa4f0f9063653806
fightagainstdev <143303788+fightagainstdev@users.noreply.github.com>
2025-09-27 15:19:55 +0800

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,199 @@
+export const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
+
+export const shapeElements = [
+  {
+    icon: "/assets/rectangle.svg",
+    name: "方形",
+    value: "rectangle",
+  },
+  {
+    icon: "/assets/circle.svg",
+    name: "圆",
+    value: "circle",
+  },
+  {
+    icon: "/assets/triangle.svg",
+    name: "三角形",
+    value: "triangle",
+  },
+  {
+    icon: "/assets/line.svg",
+    name: "线条",
+    value: "line",
+  },
+  {
+    icon: "/assets/image.svg",
+    name: "图像",
+    value: "image",
+  },
+  {
+    icon: "/assets/freeform.svg",
+    name: "自由绘制",
+    value: "freeform",
+  },
+];
+
+export const navElements = [
+  {
+    icon: "/assets/select.svg",
+    name: "选择",
+    value: "select",
+  },
+  {
+    icon: "/assets/rectangle.svg",
+    name: "方形",
+    value: shapeElements,
+  },
+  {
+    icon: "/assets/text.svg",
+    value: "文字",
+    name: "Text",
+  },
+  {
+    icon: "/assets/delete.svg",
+    value: "删除",
+    name: "Delete",
+  },
+  {
+    icon: "/assets/reset.svg",
+    value: "清空",
+    name: "Reset",
+  },
+  {
+    icon: "/assets/comments.svg",
+    value: "comments",
+    name: "Comments",
+  },
+];
+
+export const defaultNavElement = {
+  icon: "/assets/select.svg",
+  name: "评论",
+  value: "select",
+};
+
+export const directionOptions = [
+  { label: "Bring to Front", value: "front", icon: "/assets/front.svg" },
+  { label: "Send to Back", value: "back", icon: "/assets/back.svg" },
+];
+
+export const fontFamilyOptions = [
+  { value: "Helvetica", label: "Helvetica" },
+  { value: "Times New Roman", label: "Times New Roman" },
+  { value: "Comic Sans MS", label: "Comic Sans MS" },
+  { value: "Brush Script MT", label: "Brush Script MT" },
+];
+
+export const fontSizeOptions = [
+  {
+    value: "10",
+    label: "10",
+  },
+  {
+    value: "12",
+    label: "12",
+  },
+  {
+    value: "14",
+    label: "14",
+  },
+  {
+    value: "16",
+    label: "16",
+  },
+  {
+    value: "18",
+    label: "18",
+  },
+  {
+    value: "20",
+    label: "20",
+  },
+  {
+    value: "22",
+    label: "22",
+  },
+  {
+    value: "24",
+    label: "24",
+  },
+  {
+    value: "26",
+    label: "26",
+  },
+  {
+    value: "28",
+    label: "28",
+  },
+  {
+    value: "30",
+    label: "30",
+  },
+  {
+    value: "32",
+    label: "32",
+  },
+  {
+    value: "34",
+    label: "34",
+  },
+  {
+    value: "36",
+    label: "36",
+  },
+];
+
+export const fontWeightOptions = [
+  {
+    value: "400",
+    label: "正常",
+  },
+  {
+    value: "500",
+    label: "半粗体",
+  },
+  {
+    value: "600",
+    label: "粗体",
+  },
+];
+
+export const alignmentOptions = [
+  { value: "left", label: "Align Left", icon: "/assets/align-left.svg" },
+  {
+    value: "horizontalCenter",
+    label: "Align Horizontal Center",
+    icon: "/assets/align-horizontal-center.svg",
+  },
+  { value: "right", label: "Align Right", icon: "/assets/align-right.svg" },
+  { value: "top", label: "Align Top", icon: "/assets/align-top.svg" },
+  {
+    value: "verticalCenter",
+    label: "Align Vertical Center",
+    icon: "/assets/align-vertical-center.svg",
+  },
+  { value: "bottom", label: "Align Bottom", icon: "/assets/align-bottom.svg" },
+];
+
+export const shortcuts = [
+  {
+    key: "1",
+    name: "Chat",
+    shortcut: "/",
+  },
+  {
+    key: "2",
+    name: "Undo",
+    shortcut: "⌘ + Z",
+  },
+  {
+    key: "3",
+    name: "Redo",
+    shortcut: "⌘ + Y",
+  },
+  {
+    key: "4",
+    name: "Reactions",
+    shortcut: "E",
+  },
+];
